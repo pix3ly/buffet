c04d5ebfbd3458c98a44f5f0dc215b90a97975e2
Daniël van Drunen <daniel@pixely.me>
2017-09-04 19:40:08 +0200

    Fixed bug where coin names were case sensitive (also replaced emojis)

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ client.on('message', msg => {
     const parts = msg.content.split(' ')
 
     if (parts[0] === '.price') {
-        axios.get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=BTC-' + parts[1]).then((response) => {
+        const coin = parts[1].toUpperCase()
+
+        axios.get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=BTC-' + coin).then((response) => {
             const json = response.data
 
             if (json.success) {
@@ -26,23 +28,23 @@ client.on('message', msg => {
 
                     if (json.success) {
                         const currentPrice = Number(json.result[0].Last * btcValue).toFixed(2)
-                        const oldPrice = priceCache[parts[1]]
+                        const oldPrice = priceCache[coin]
 
-                        let response = parts[1] + ' is currently ' + currentPrice + ' USD'
+                        let response = coin + ' is currently ' + currentPrice + ' USD'
 
                         if (oldPrice) {
                             let emoji = ''
 
                             if (currentPrice > oldPrice) {
-                                emoji += ' :rocket:'
+                                emoji += ' :chart_with_downwards_trend:'
                             } else if (currentPrice < oldPrice) {
-                                emoji += ' :red_circle:'
+                                emoji += ' :chart_with_upwards_trend:'
                             }
 
                             response += ' (was previously ' + oldPrice + ' USD' + emoji + ')'
                         }
 
-                        priceCache[parts[1]] = currentPrice
+                        priceCache[coin] = currentPrice
 
                         msg.reply(response)
                     }
