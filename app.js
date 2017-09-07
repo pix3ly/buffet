57956eff2e067ed037c80509871a1ca82e4c1e6c
Daniël van Drunen <daniel@pixely.me>
2017-09-07 20:01:51 +0200

    Replace loose API requests for price retrieval with getPrice function

--- app.js
+++ app.js
@@ -5,9 +5,30 @@ const axios = require('axios')
 
 const client = new Discord.Client()
 
-const priceCache = []
 const trackingHistory = []
 const trackingCoins = ['ETH', 'MTL', 'GNT']
+
+function getPrice(currency, callback) {
+    axios.get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=BTC-' + currency)
+        .then((response) => {
+            const json = response.data
+
+            if (json.success) {
+                const btcPrice = json.result[0].Last
+
+                axios.get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=USDT-BTC')
+                    .then((response) => {
+                        const json = response.data
+
+                        const usdPrice = Number(json.result[0].Last * btcPrice).toFixed(2)
+
+                        callback(false, usdPrice)
+                    })
+            } else {
+                callback(true, null)
+            }
+        })
+}
 
 client.on('message', msg => {
     if (msg.content === '.ping') {
@@ -17,40 +38,11 @@ client.on('message', msg => {
     const parts = msg.content.split(' ')
 
     if (parts[0] === '.price') {
-        const coin = parts[1].toUpperCase()
+        const currency = parts[1].toUpperCase()
 
-        axios.get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=BTC-' + coin).then((response) => {
-            const json = response.data
-
-            if (json.success) {
-                const btcValue = json.result[0].Last
-
-                axios.get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=USDT-BTC').then((response) => {
-                    const json = response.data
-
-                    if (json.success) {
-                        const currentPrice = Number(json.result[0].Last * btcValue).toFixed(2)
-                        const oldPrice = priceCache[coin]
-
-                        let response = coin + ' is currently ' + currentPrice + ' USD'
-
-                        if (oldPrice) {
-                            let emoji = ''
-
-                            if (currentPrice > oldPrice) {
-                                emoji += ' :chart_with_downwards_trend:'
-                            } else if (currentPrice < oldPrice) {
-                                emoji += ' :chart_with_upwards_trend:'
-                            }
-
-                            response += ' (was previously ' + oldPrice + ' USD' + emoji + ')'
-                        }
-
-                        priceCache[coin] = currentPrice
-
-                        msg.reply(response)
-                    }
-                })
+        getPrice(currency, (error, usd) => {
+            if (!error) {
+                msg.reply(currency + ' is currently ' + usd + ' USD');
             } else {
                 msg.reply('there\'s no currency with that name')
             }
@@ -60,39 +52,25 @@ client.on('message', msg => {
 
 setInterval(() => {
     trackingCoins.forEach((coin) => {
-        axios.get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=BTC-' + coin)
-            .then((response) => {
-                const json = response.data
+        getPrice(coin, (error, usd) => {
+            if (!error) {
+                if (trackingHistory[coin] > usd) {
+                    const percentage = Number(trackingHistory[coin] / usd * 100).toFixed(1) - 100
 
-                if (json.success) {
-                    const btc = json.result[0].Last
+                    if (percentage >= 5) {
+                        client.channels.get(config.alertsChannel).sendMessage(coin + ' is going up by ' + percentage + '%')
+                    }
+                } else if (trackingHistory[coin] < usd) {
+                    const percentage = Number(usd / trackingHistory[coin] * 100).toFixed(1) - 100
 
-                    axios.get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=USDT-BTC')
-                        .then((response) => {
-                            const json = response.data
-
-                            if (json.success) {
-                                const usd = Number(json.result[0].Last * btc).toFixed(2)
-
-                                if (trackingHistory[coin] > usd) {
-                                    const percentage = Number(trackingHistory[coin] / usd * 100).toFixed(1) - 100
-
-                                    if (percentage >= 5) {
-                                        client.channels.get(config.alertsChannel).sendMessage(coin + ' is going up by ' + percentage + '%')
-                                    }
-                                } else if (trackingHistory[coin] < usd) {
-                                    const percentage = Number(usd / trackingHistory[coin] * 100).toFixed(1) - 100
-
-                                    if (percentage >= 5) {
-                                        client.channels.get(config.alertsChannel).sendMessage(coin + ' is going down by ' + percentage + '%')
-                                    }
-                                }
-
-                                trackingHistory[coin] = usd
-                            }
-                        })
+                    if (percentage >= 5) {
+                        client.channels.get(config.alertsChannel).sendMessage(coin + ' is going down by ' + percentage + '%')
+                    }
                 }
-            })
+
+                trackingHistory[coin] = usd
+            }
+        })
     })
 }, 1000 * 60 * 5)
 
