706f8565d203c1debb0c693921d6ee858677e039
Daniël van Drunen <daniel@pixely.me>
2017-09-07 18:16:27 +0200

    Add price tracking

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const axios = require('axios')
 const client = new Discord.Client()
 
 const priceCache = []
+const trackingHistory = []
+const trackingCoins = ['ETH', 'MTL', 'GNT']
 
 client.on('message', msg => {
     if (msg.content === '.ping') {
@@ -55,5 +57,43 @@ client.on('message', msg => {
         })
     }
 })
+
+setInterval(() => {
+    trackingCoins.forEach((coin) => {
+        axios.get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=BTC-' + coin)
+            .then((response) => {
+                const json = response.data
+
+                if (json.success) {
+                    const btc = json.result[0].Last
+
+                    axios.get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=USDT-BTC')
+                        .then((response) => {
+                            const json = response.data
+
+                            if (json.success) {
+                                const usd = Number(json.result[0].Last * btc).toFixed(2)
+
+                                if (trackingHistory[coin] > usd) {
+                                    const percentage = Number(trackingHistory[coin] / usd * 100).toFixed(1) - 100
+
+                                    if (percentage >= 5) {
+                                        client.channels.get(config.alertsChannel).sendMessage(coin + ' is going up by ' + percentage + '%')
+                                    }
+                                } else if (trackingHistory[coin] < usd) {
+                                    const percentage = Number(usd / trackingHistory[coin] * 100).toFixed(1) - 100
+
+                                    if (percentage >= 5) {
+                                        client.channels.get(config.alertsChannel).sendMessage(coin + ' is going down by ' + percentage + '%')
+                                    }
+                                }
+
+                                trackingHistory[coin] = usd
+                            }
+                        })
+                }
+            })
+    })
+}, 1000 * 60 * 5)
 
 client.login(config.token)
