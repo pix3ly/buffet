7bfefcdad7b542ac89595c3e2ad6eeaf100d3e7a
Daniël van Drunen <daniel@pixely.me>
2017-09-16 17:13:42 +0200

    Get rid of price alerts (for now)

--- app.js
+++ app.js
@@ -5,14 +5,6 @@ const axios = require('axios')
 
 const client = new Discord.Client()
 
-const cache = []
-const tracking = []
-
-tracking['ETH'] = {}
-tracking['GNT'] = {}
-tracking['MTL'] = {}
-tracking['NEO'] = {}
-
 client.on('message', msg => {
     const startingCharacter = msg.content.substr(0, 1)
 
@@ -35,38 +27,4 @@ client.on('message', msg => {
     }
 })
 
-const getPrice = require('./helpers/get_price')
-
-function track() {
-    for (const key in tracking) {
-        const value = tracking[key]
-
-        getPrice(key, (error, usd) => {
-            if (!error) {
-                if (value.price) {
-                    let change = Number((usd - value.price) / value.price * 100).toFixed(1)
-
-                    let broadcast = false
-
-                    if (change >= config.alerts.margin) {
-                        broadcast = key + ' is going up by ' + change + '%'
-                    } else if (change <= -config.alerts.margin) {
-                        broadcast = key + ' is going down by ' + change + '%'
-                    }
-
-                    if (broadcast) {
-                        client.channels.get(config.alerts.channel).sendMessage(broadcast)
-                    }
-                }
-
-                tracking[key].price = usd
-            }
-        })
-    }
-}
-
 client.login(config.token)
-
-track()
-
-setInterval(track, 1000 * 60 * 5)
--- config.example.js
+++ config.example.js
@@ -1,9 +1,5 @@
 module.exports = {
     token: '',
-    alerts: {
-        channel: '',
-        margin: 2
-    },
     commands: {
         ping: require('./commands/ping'),
         price: require('./commands/price')
